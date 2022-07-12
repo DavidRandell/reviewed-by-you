043b12061a8f65dd81934729d5ad2511e9ec7750
David Randell <davidrandell@Davids-Mac-mini.local>
2022-07-12 23:14:34 +0100

    Fixing routes

--- application.py
+++ application.py
@@ -1,9 +1,10 @@
 from flask import Flask, request
 application = Flask(__name__, static_url_path='')
+app = application
 
 @application.route('/html/')
 def root():
-    return app.send_static_file('index.html')
+    return app.send_static_file('/html/index.html')
 
 if __name__ == '__main__':
     application.run(debug=True)
