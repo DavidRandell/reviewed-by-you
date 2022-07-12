7f694c67106404d10df6dfb204a8c798ea7118ff
David Randell <davidrandell@Davids-Mac-mini.local>
2022-07-12 22:48:53 +0100

    Change to string

--- application.py
+++ application.py
@@ -3,4 +3,4 @@ application = Flask(__name__)
 
 @application.route('/')
 def hello_world():
-    return 'Hello World'
+    return 'Hello World, this is deployed to Elastic Beanstalk through an AWS pipeline'
