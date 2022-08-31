f31039e6435df86605c49d1bc77e7d59236f925e
David Randell <david@davidrandell.com>
2022-08-31 23:06:16 +0100

    Testing changes

--- static/js/main.js
+++ static/js/main.js
@@ -3,7 +3,13 @@ const API_URL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.
 const IMG_PATH = 'https://image.tmdb.org/t/p/w1280';
 const SEARCH_API = 'https://api.themoviedb.org/3/search/movie?api_key=dc3959e18dc07cab7733cd17f3f2eb39&query="';
 
+const SHOW_API = 'https://api.themoviedb.org/3/movie/';
+
+const api_key = 'dc3959e18dc07cab7733cd17f3f2eb39'
+
 const form = document.getElementById('form');
+
+
 
 // Get initial movies
 
@@ -16,15 +22,19 @@ async function getMovies(url) {
     showMovies(data.results)
 }
 
+function getMovie(movie) {
+    console.log("here!" + this)
+}
+
 function showMovies(movies) {
     main.innerHTML = ''
 
     movies.forEach((movie) => {
-        const { title, poster_path, vote_average, overview } = movie
+        const { id, title, poster_path, vote_average, overview } = movie
     
         const movieEl = document.createElement('div')
         movieEl.classList.add('movie')
-        movieEl.innerHTML = `<img src="${IMG_PATH + poster_path}" alt="${title}" /><div class="movie-info"><h3>${title}</h3><span class="${getClassByRate(vote_average)}">${vote_average}</span></div><div class="overview"><h3>Overview</h3><p>${overview}</p></div>
+        movieEl.innerHTML = `<a href="${SHOW_API + id}?api_key=${api_key}&language=en-US" class="movieLink" style="display: block;" ><img src="${IMG_PATH + poster_path}" alt="${title}" /></a><div class="movie-info"><h3>${title}</h3><span class="${getClassByRate(vote_average)}">${vote_average}</span></div><div class="overview"><h3>Overview</h3><p>${overview}</p></div>
         `
         main.appendChild(movieEl)
     
@@ -53,4 +63,10 @@ form.addEventListener('submit', (e) => {
     } else {
         window.location.reload();
     }
+})
+
+document.getElementsByClassName('movieLink').addEventListener('click', (e) => {
+    e.preventDefault()
+    console.log("clicked")
+    
 })
